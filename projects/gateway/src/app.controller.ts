d3d29319e065ee33c315d6f87e8ec3726d3f4af5
CatsJuice <wp1298554944@hotmail.com>
2023-08-02 16:02:52 +0800

    fix(client): build error

--- projects/gateway/src/app.controller.ts
+++ projects/gateway/src/app.controller.ts
@@ -1,17 +1,8 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller } from '@nestjs/common'
 
-import { ErrorCode } from 'zjf-types'
 import { AppService } from './app.service'
-import { ApiErrorResponse, ApiSuccessResponse } from './utils/response'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @ApiErrorResponse(ErrorCode.AUTH_LOGIN_EXPIRED, ErrorCode.AUTH_LOGIN_REQUIRED)
-  @ApiSuccessResponse(String)
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
-  }
 }
