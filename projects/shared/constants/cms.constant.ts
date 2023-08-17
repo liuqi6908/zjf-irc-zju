61ee6ed093d8ac66b4cc021c6cb8366b500c0cba
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-08-17 16:04:59 +0800

    feat(shared):cms配置

--- projects/shared/constants/cms.constant.ts
+++ projects/shared/constants/cms.constant.ts
@@ -1,10 +1,29 @@
+import { defineAsyncComponent } from 'vue';
+const HomeCarousel = defineAsyncComponent(() => import('../component/cms/home/HomeCarousel.vue'))
+
 export const cmsConfig = [
     {
-        id: 'homeCarousel',
-        component: () => import('../component/HomeCarousel.vue')
-    },
-    {
-        id: 'homeQuestion',
-        component: () => import('../component/HomeQuestion.vue')
+        parentId: '',
+        id:'home',
+        label: '',
+        children: [
+            {
+                id: 'homeCarousel',
+                label: '首页轮播图',
+                parentId: 'home',
+                component: HomeCarousel,
+                /** 当前轮播图所需要的操作，表头按照从左到右的顺序 */
+                col: ['title', 'content', 'uploadImg', 'delete'],
+                rows: []
+            },
+            {
+                id: 'homeQuestion',
+                label: '首页问卷',
+                parentId: 'home',
+                component: defineAsyncComponent(() => import('../component/cms/home/HomeQuestion.vue')),
+                col: ['title', 'content', 'delete'],
+                rows: []
+            }
+        ]
     }
 ]
