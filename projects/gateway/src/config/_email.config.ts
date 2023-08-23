c8220ac4af2361da223c81f8854847051c229a17
CatsJuice <wp1298554944@hotmail.com>
2023-08-23 19:05:06 +0800

    feat(gateway/mail): replace resend with nodemailer(smtp)

--- projects/gateway/src/config/_email.config.ts
+++ projects/gateway/src/config/_email.config.ts
@@ -2,4 +2,13 @@ import { registerAs } from '@nestjs/config'
 
 export default registerAs('email', () => ({
   resendApiKey: process.env.EMAIL_RESEND_API_KEY,
+  smtp: {
+    host: process.env.EMAIL_SMTP_HOST,
+    port: Number.parseInt(process.env.EMAIL_SMTP_PORT, 10),
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_SMTP_USER,
+      pass: process.env.EMAIL_SMTP_PSWD,
+    },
+  },
 }))
--- projects/gateway/src/modules/email/email.service.ts
+++ projects/gateway/src/modules/email/email.service.ts
@@ -1,27 +1,31 @@
 import { Resend } from 'resend'
+import * as nodemailer from 'nodemailer'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import type { OnModuleInit } from '@nestjs/common'
 
 import { CodeService } from '../code/code.service'
+
 import type { LoginByEmailLinkDto } from '../auth/dto/login-by-email-link.body.dto'
 import type { SendEmailCodeBodyDto } from './dto/send-email-code.body.dto'
 
 @Injectable()
 export class EmailService implements OnModuleInit {
   readonly resend: Resend
+  readonly transporter: nodemailer.Transporter
 
   constructor(
     private readonly _cfgSrv: ConfigService,
     private readonly _codeSrv: CodeService,
   ) {
     this.resend = new Resend(_cfgSrv.get<string>('email.resendApiKey'))
+    this.transporter = nodemailer.createTransport(_cfgSrv.get('email.smtp'))
   }
 
   onModuleInit() {}
 
   getClient() {
-    return this.resend
+    return this.transporter
   }
 
   public async sendCode(body: SendEmailCodeBodyDto) {
@@ -30,9 +34,9 @@ export class EmailService implements OnModuleInit {
     const { code, bizId } = await this._codeSrv.createCode(email, action, expInMin)
     const subject = `ZJF ${action}`
     const html = `<p>Your code for ${action} is: <strong>${code}</strong>, expire in ${expInMin} minutes</p>`
-    this.resend.emails.send({
-      from: 'CatsJuice <catsjuice@oooo.so>',
-      to: [email],
+    this.transporter.sendMail({
+      from: 'NoReplay <noreplay@qiyandata.com>',
+      to: email,
       subject,
       html,
     }).catch(console.error)
@@ -41,9 +45,9 @@ export class EmailService implements OnModuleInit {
 
   public async sendMagicLink(body: LoginByEmailLinkDto, token: string) {
     const href = `${body.redirect}${body.redirect.includes('?') ? '&' : '?'}${body.queryName || 'token'}=${token}`
-    this.resend.emails.send({
-      from: 'CatsJuice <catsjuice@oooo.so>',
-      to: [body.email],
+    this.transporter.sendMail({
+      from: 'NoReplay <noreplay@qiyandata.com>',
+      to: body.email,
       subject: 'ZJF Login',
       html: `<a href="${href}">Click here to Login</a>, or copy this link to your browser: ${href}`,
     })
