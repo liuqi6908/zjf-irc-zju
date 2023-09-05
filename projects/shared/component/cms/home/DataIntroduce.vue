15d22d7582042e7fb25ac59f59ad1a8c052fbcda
CatsJuice <wp1298554944@hotmail.com>
2023-09-05 18:47:01 +0800

    fix(client): 首页第一个问题垂直居中，设置默认字体大小

--- projects/shared/component/cms/home/DataIntroduce.vue
+++ projects/shared/component/cms/home/DataIntroduce.vue
@@ -12,12 +12,12 @@ defineProps<Props>()
 
 <template>
     <div w-full bg-grey-1  flex-center py-20 >
-        <div flex="~ row gap-20">
+        <div flex="~ row" gap-20 items-center>
             <div h-60 w-80 class="introduce_bg-img" /> 
             <div max-w-xl class="col-grow" v-if="list && list.length">
                 <div title-1 text-grey-8>{{ list[0].title }}</div>
                 <br/>
-                <div indent-1 text-grey-8 v-html="list[0].richText" />
+                <div text-4.5 indent-1 text-grey-8 v-html="list[0].richText" />
             </div>
         </div>
     </div>
