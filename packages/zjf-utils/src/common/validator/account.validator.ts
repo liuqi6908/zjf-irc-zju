024c6305ee03e830bc2df58c4917687e6faa1bf8
CatsJuice <wp1298554944@hotmail.com>
2023-09-13 17:30:39 +0800

    chore(constant): 账号最小长度限制为2

--- packages/zjf-utils/src/common/validator/account.validator.ts
+++ packages/zjf-utils/src/common/validator/account.validator.ts
@@ -1,6 +1,6 @@
 export const ROOT_ACCOUNT = "zjf_root_admin"
 export const ACCOUNT_MAX_LENGTH = 16
-export const ACCOUNT_MIN_LENGTH = 4
+export const ACCOUNT_MIN_LENGTH = 2
 export const ACCOUNT_ALLOW_CHARS = `_`
 export const ACCOUNT_REQUIREMENTS_DESC = `账号长度不得小于 ${ACCOUNT_MIN_LENGTH} 位，不得大于 ${ACCOUNT_MAX_LENGTH} 位, 由数字，字母和下划线组成`
 
