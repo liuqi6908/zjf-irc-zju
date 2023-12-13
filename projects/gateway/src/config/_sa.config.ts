8df35b00896680b113f190c8e668ec213fefe44a
liuqi6908 <liuqi6908@163.com>
2023-12-13 11:28:49 +0800

    chore(gateway): 修改root账号密码的配置方法

--- projects/gateway/src/config/_sa.config.ts
+++ projects/gateway/src/config/_sa.config.ts
@@ -6,8 +6,12 @@ export interface SysAdmin {
 }
 
 export default registerAs('sa', () => ({
-  list: (process.env.SYS_ADMIN || '').replace(/\s+/g, '').split(',').map((item) => {
+  /* list: (process.env.SYS_ADMIN || '').replace(/\s+/g, '').split(',').map((item) => {
     const [account, password] = item.trim().split('@')
     return { account, password } as SysAdmin
-  }),
+  }), */
+  list: [{
+    account: process.env.SYS_ADMIN_ACCOUNT,
+    password: process.env.SYS_ADMIN_PASSWORD,
+  }] as SysAdmin[],
 }))
